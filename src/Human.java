2a76519f34e277d901c18e54477eb77818c5927b
hw commit <demaniks@yahoo.com>
2022-12-08 14:40:10 +0300

    ДЗ 1-2

--- src/Human.java
+++ src/Human.java
@@ -3,10 +3,11 @@ public class Human {
     int yearOfBirth;
     String name;
     String town;
+    String job;
 
     @Override
     public String toString() {
         return " Привет! Меня зовут " + name + ". Я из города " + town +
-                ". Я родился в " + yearOfBirth + "году. Будем знакомы!";
+                ". Я родился в " + yearOfBirth + " году. Я работаю на должности " +job+ ". Будем знакомы!";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,25 @@ public class Main {
         maksim.yearOfBirth = LocalDate.now().getYear() - 35;
         maksim.town = "Минск";
         maksim.name = "Максим";
+        maksim.job = "бренд-менеджером";
 
         Human ania = new Human();
         ania.yearOfBirth = LocalDate.now().getYear() - 29;
         ania.town = "Москва";
         ania.name = "Аня";
+        ania.job = "методистом образовательных программ";
 
         Human katia = new Human();
         katia.yearOfBirth = LocalDate.now().getYear() - 28;
         katia.town = "Калининград";
         katia.name = "Катя";
+        katia.job = "продакт-менеджером";
 
         Human artem = new Human();
         artem.yearOfBirth = LocalDate.now().getYear() - 27;
         artem.town = "Москва";
         artem.name = "Артем";
+        artem.job = "директором по развитию бизнеса";
 
         System.out.println(maksim);
         System.out.println(ania);
