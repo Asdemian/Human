404931c346d99b94d00a825411a9a520fad50a49
hw commit <demaniks@yahoo.com>
2022-12-08 20:03:35 +0300

    ДЗ (часть 2) 1-1

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
         Human maksim = new Human(LocalDate.now().getYear() - 35,
-                "Минск", "Максим", "бренд-менеджером");
+                "Максим", "Минск", "бренд-менеджером");
 
         Human ania = new Human(LocalDate.now().getYear() - 29,
-                "Москва", "Аня",
+                "Аня", "Москва",
                 "методистом образовательных программ");
 
         Human katia = new Human(LocalDate.now().getYear() - 28,
-                "Калининград", "Катя", "продакт-менеджером");
+                "Катя", "Калининград", "продакт-менеджером");
 
         Human artem = new Human(LocalDate.now().getYear() - 27,
-                "Москва", "Артем",
+                "Артем", "Москва",
                 "директором по развитию бизнеса");
 
         Human vladimir = new Human(LocalDate.now().getYear() - 21,
-                "Казань", "Владимир",
+                "Владимир", "Казань",
                 null);
 
         System.out.println(maksim);
