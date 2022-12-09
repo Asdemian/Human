a041f62fa81feb1eac0abe38629ed92c403c62b5
hw commit <demaniks@yahoo.com>
2022-12-09 13:14:44 +0300

    ДЗ (часть 2) 1-2

--- src/Flower.java
+++ src/Flower.java
@@ -1,5 +1,64 @@
 public class Flower {
 
+    private final String flowerName;
+    private String country;
+    private final double cost;
+    private int lifeSpan;
+
+    public Flower(String flowerName, String country, double cost) {
+        if (flowerName == null || flowerName.isEmpty()) {
+            this.flowerName = "тип не указан";
+        } else {
+            this.flowerName = flowerName;
+        }
+
+        if (country == null || country.isEmpty()) {
+            this.country = "Россия";
+        } else {
+            this.country = country;
+        }
+        if (cost <= 0) {
+            cost = 1;
+        }
+        this.cost = cost;
+        this.lifeSpan = 3;
+    }
 
 
+    public String getFlowerName() {
+        return flowerName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getLifeSpan() {
+        return lifeSpan;
+    }
+
+    public void setLifeSpan(int lifeSpan) {
+        if (lifeSpan > 0) {
+            this.lifeSpan = lifeSpan;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Цветок (" +
+                " наименование: " + flowerName + ')' +
+                " страны " + country +
+                " стоимость " + cost +
+                " руб. срок стояния " + lifeSpan +
+                " дн.)";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -20,11 +20,26 @@ public class Main {
         Human vladimir = new Human(LocalDate.now().getYear() - 21,
                 "Владимир", "Казань",
                 null);
-
         System.out.println(maksim);
         System.out.println(ania);
         System.out.println(katia);
         System.out.println(artem);
         System.out.println(vladimir);
+
+        Flower rosa = new Flower("Роза", "Голандии", 35.59);
+        Flower chrysanthemum = new Flower("Хризантема", null, 15);
+        chrysanthemum.setLifeSpan(5);
+        Flower pion = new Flower("Пион", "Англия", 69.9);
+        pion.setLifeSpan(1);
+        Flower gypsophila = new Flower("Гипсофила", "Турция", 19.5);
+        gypsophila.setLifeSpan(10);
+
+        Bouquet bouquet = new Bouquet(
+                new Flower[]{
+                        rosa, rosa, rosa, chrysanthemum, chrysanthemum, chrysanthemum,
+                        chrysanthemum, chrysanthemum, gypsophila
+                }
+        );
+        bouquet.bouquetOfFlowers();
     }
 }
